264908995121042248c364c4dce9c1e86bd48023
tomdelie <tomdelie78@gmail.com>
2024-12-07 18:13:38 +0100

    feat: add animations

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# testdadadad
+# Weather
--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+const form = document.querySelector('form.search');
 const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
 const weatherCard = document.querySelector('.weather-card');
 const weatherApiKey = "cf78ba9982e3863f6daf7a79c270cd0b";
 
-const cityName = document.getElementById('city');
-const temp = document.getElementById('temp');
+const humidity = document.querySelector('.humidity-value');
+const wind = document.querySelector('.wind-value');
+const temp = document.querySelector('.temp-value');
 const icon = document.getElementById('icon');
 
-searchButton.addEventListener('click', () => {
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
   if (searchInput.value) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&APPID=${weatherApiKey}`)
     .then(response => {
@@ -17,18 +20,19 @@ searchButton.addEventListener('click', () => {
       return response.json();
     })
     .then(data => {
-      cityName.innerText = data.name;
-      temp.innerText = `${Math.round(data.main.temp)}°`;
+      humidity.innerText = data.main.humidity;
+      wind.innerText = Math.round(data.wind.speed);
+      temp.innerText = Math.round(data.main.temp);
       if (data.weather.length) {
-        icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
       } else {
-        icon.src = `https://openweathermap.org/img/wn/02d@2x.png`;
+        icon.src = `https://openweathermap.org/img/wn/02d@4x.png`;
       }
-
-      weatherCard.classList.remove('hidden');
     })
-    .catch(error => {
-      console.error('Error:', error);
+    .catch(() => {
+      humidity.innerText = '-';
+      wind.innerText = '-';
+      temp.innerText = '-';
     });
   }
 });
